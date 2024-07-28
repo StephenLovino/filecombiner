30b9db16e41d7cc66eec9878fbe2b25b7eb4fa43
Stephen Jan Lovino <stephen.ben19@gmail.com>
2024-07-28 15:19:23 +0000

    Fix Dockerfile for Android SDK setup

--- install-android-sdk.sh
+++ install-android-sdk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Ensure Android SDK is properly installed
-yes | ${ANDROID_HOME}/cmdline-tools/tools/bin/sdkmanager --licenses
-${ANDROID_HOME}/cmdline-tools/tools/bin/sdkmanager "platform-tools" "platforms;android-30" "build-tools;30.0.3"
+yes | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager --licenses
+$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager "platform-tools" "platforms;android-30" "build-tools;30.0.3"
